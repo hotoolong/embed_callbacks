388d6c1856031dd2c225820acdf48d8ad5b383a0
hotoolong <hotoolong.hogehoge+github@gmail.com>
2020-07-30 16:26:41 +0900

    generate gem files

---
+++ lib/embed_callbacks.rb
@@ -0,0 +1,6 @@
+require "embed_callbacks/version"
+
+module EmbedCallbacks
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/embed_callbacks/version.rb
@@ -0,0 +1,3 @@
+module EmbedCallbacks
+  VERSION = "0.1.0"
+end

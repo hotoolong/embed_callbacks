11022109532c94859eb26d818b4c5ba25c3ed7a5
hotoolong <hotoolong.hogehoge+github@gmail.com>
2020-08-18 23:46:57 +0900

    add condition

--- lib/embed_callbacks.rb
+++ lib/embed_callbacks.rb
@@ -6,23 +6,63 @@ module EmbedCallbacks
   end
 
   module PrependMethods
-    def set_callback(target_method_name, behavior, callback_function_name)
-      raise ArgumentError, 'The behavior should be set in the before after around rescue' unless %i(before after around rescue ensure).include?(behavior)
+    def set_callback(target_method_name, behavior_sym, callback_function_name, **options)
+      behavior = Behavior.new(behavior_sym)
       m = Module.new
       m.define_method(target_method_name) do |*params|
+        condition = Condition.new(options).call(self)
         begin
-          method(callback_function_name).call if %i(before around).include?(behavior)
+          method(callback_function_name).call if condition && behavior.before?
           return_value = super(*params)
-          method(callback_function_name).call if %i(after around).include?(behavior)
+          method(callback_function_name).call if condition && behavior.after?
           return_value
         rescue => e
-          method(callback_function_name).call if behavior == :rescue
+          method(callback_function_name).call if condition && behavior.rescue?
           raise e
         ensure
-          method(callback_function_name).call if behavior == :ensure
+          method(callback_function_name).call if condition && behavior.ensure?
         end
       end
       prepend m
+    end
+  end
+
+  class Condition
+    def initialize(options)
+      @if = options[:if]
+      @unless = options[:unless]
+      raise ArgumentError, "Don't use if and unless at the same time." if @if && @unless
+    end
+
+    def call(record)
+      return @if.call(record) if @if 
+      return !@unless&.call(record) if @unless
+      true
+    end
+  end
+
+  class Behavior
+    KINDS = %i(before after around rescue ensure)
+
+    def initialize(behavior)
+      @behavior = behavior
+      raise ArgumentError, 'The behavior should be set in the before after around rescue' unless KINDS.include?(behavior)
+    end
+
+    def before?
+      %i(before around).include?(@behavior)
+    end
+
+    def after?
+      %i(after around).include?(@behavior)
+    end
+
+    def rescue?
+      :rescue == @behavior
+    end
+
+    def ensure?
+      :ensure == @behavior
     end
   end
 end
--- spec/embed_callbacks_spec.rb
+++ spec/embed_callbacks_spec.rb
@@ -254,4 +254,206 @@ RSpec.describe EmbedCallbacks do
       expect('target3testcallback1callback2').to eq(callback.buffer)
     end
   end
+
+  context 'condition if' do
+    class ConditionIfCallbackClass
+      include EmbedCallbacks
+      attr_accessor :buffer
+      attr_accessor :check_flag
+
+      set_callback :target1, :before, :callback1, if: ->(record) { record.check_flag }
+      set_callback :target2, :after,  :callback1, if: ->(record) { record.check_flag }
+      set_callback :target3, :rescue, :callback1, if: ->(record) { record.check_flag }
+      set_callback :target4, :ensure, :callback2, if: ->(record) { record.check_flag }
+
+      def initialize
+        @buffer = ''
+      end
+
+      def target1
+        @buffer += 'target1'
+      end
+
+      def target2(arg)
+        @buffer += 'target2' + arg
+      end
+
+      def target3(arg)
+        @buffer += 'target3' + arg
+        raise 'test'
+      end
+
+      def target4
+        @buffer += 'target4'
+        raise 'test'
+      end
+
+      private
+
+      def callback1
+        @buffer += 'callback1'
+      end
+
+      def callback2
+        @buffer += 'callback2'
+      end
+    end
+
+    it "before callback and condition if false" do
+      callback = ConditionIfCallbackClass.new
+      callback.check_flag = false 
+      callback.target1
+      expect('target1').to eq(callback.buffer)
+    end
+
+    it "before callback and condition if true" do
+      callback = ConditionIfCallbackClass.new
+      callback.check_flag = true
+      callback.target1
+      expect('callback1target1').to eq(callback.buffer)
+    end
+
+    it "after callback and condition if false" do
+      callback = ConditionIfCallbackClass.new
+      callback.check_flag = false 
+      callback.target2('condition_false')
+      expect('target2condition_false').to eq(callback.buffer)
+    end
+
+    it "after callback and condition if true" do
+      callback = ConditionIfCallbackClass.new
+      callback.check_flag = true
+      callback.target2('condition_true')
+      expect('target2condition_truecallback1').to eq(callback.buffer)
+    end
+
+    it "rescue callback and condition if false" do
+      callback = ConditionIfCallbackClass.new
+      callback.check_flag = false 
+      expect { callback.target3('condition_false') }.to raise_error(RuntimeError)
+      expect('target3condition_false').to eq(callback.buffer)
+    end
+
+    it "rescue callback and condition if true" do
+      callback = ConditionIfCallbackClass.new
+      callback.check_flag = true
+      expect { callback.target3('condition_true') }.to raise_error(RuntimeError)
+      expect('target3condition_truecallback1').to eq(callback.buffer)
+    end
+
+    it "ensure callback and condition if false" do
+      callback = ConditionIfCallbackClass.new
+      callback.check_flag = false 
+      expect { callback.target4 }.to raise_error(RuntimeError)
+      expect('target4').to eq(callback.buffer)
+    end
+
+    it "ensure callback and condition if true" do
+      callback = ConditionIfCallbackClass.new
+      callback.check_flag = true
+      expect { callback.target4 }.to raise_error(RuntimeError)
+      expect('target4callback2').to eq(callback.buffer)
+    end
+  end
+
+  context 'condition unless' do
+    class ConditionUnlessCallbackClass
+      include EmbedCallbacks
+      attr_accessor :buffer
+      attr_accessor :check_flag
+
+      set_callback :target1, :before, :callback1, unless: ->(record) { record.check_flag }
+      set_callback :target2, :after,  :callback1, unless: ->(record) { record.check_flag }
+      set_callback :target3, :rescue, :callback1, unless: ->(record) { record.check_flag }
+      set_callback :target4, :ensure, :callback2, unless: ->(record) { record.check_flag }
+
+      def initialize
+        @buffer = ''
+      end
+
+      def target1
+        @buffer += 'target1'
+      end
+
+      def target2(arg)
+        @buffer += 'target2' + arg
+      end
+
+      def target3(arg)
+        @buffer += 'target3' + arg
+        raise 'test'
+      end
+
+      def target4
+        @buffer += 'target4'
+        raise 'test'
+      end
+
+      private
+
+      def callback1
+        @buffer += 'callback1'
+      end
+
+      def callback2
+        @buffer += 'callback2'
+      end
+    end
+
+    it "before callback and condition unless false" do
+      callback = ConditionUnlessCallbackClass.new
+      callback.check_flag = false 
+      callback.target1
+      expect('callback1target1').to eq(callback.buffer)
+    end
+
+    it "before callback and condition unless true" do
+      callback = ConditionUnlessCallbackClass.new
+      callback.check_flag = true
+      callback.target1
+      expect('target1').to eq(callback.buffer)
+    end
+
+    it "after callback and condition unless false" do
+      callback = ConditionUnlessCallbackClass.new
+      callback.check_flag = false 
+      callback.target2('condition_false')
+      expect('target2condition_falsecallback1').to eq(callback.buffer)
+    end
+
+    it "after callback and condition unless true" do
+      callback = ConditionUnlessCallbackClass.new
+      callback.check_flag = true
+      callback.target2('condition_true')
+      expect('target2condition_true').to eq(callback.buffer)
+    end
+
+    it "rescue callback and condition unless false" do
+      callback = ConditionUnlessCallbackClass.new
+      callback.check_flag = false 
+      expect { callback.target3('condition_false') }.to raise_error(RuntimeError)
+      expect('target3condition_falsecallback1').to eq(callback.buffer)
+    end
+
+    it "rescue callback and condition unless true" do
+      callback = ConditionUnlessCallbackClass.new
+      callback.check_flag = true
+      expect { callback.target3('condition_true') }.to raise_error(RuntimeError)
+      expect('target3condition_true').to eq(callback.buffer)
+    end
+
+    it "ensure callback and condition unless false" do
+      callback = ConditionUnlessCallbackClass.new
+      callback.check_flag = false 
+      expect { callback.target4 }.to raise_error(RuntimeError)
+      expect('target4callback2').to eq(callback.buffer)
+    end
+
+    it "ensure callback and condition unless true" do
+      callback = ConditionUnlessCallbackClass.new
+      callback.check_flag = true
+      expect { callback.target4 }.to raise_error(RuntimeError)
+      expect('target4').to eq(callback.buffer)
+    end
+  end
 end

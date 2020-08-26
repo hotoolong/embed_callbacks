3eac65bc718f429816357bb2398052371b780e03
hotoolong <hotoolong.hogehoge+github@gmail.com>
2020-08-26 22:58:34 +0900

    splitting files by class

--- lib/embed_callbacks.rb
+++ lib/embed_callbacks.rb
@@ -1,4 +1,6 @@
 require "embed_callbacks/version"
+require "embed_callbacks/condition"
+require "embed_callbacks/behavior"
 
 module EmbedCallbacks
   def self.included(base)
@@ -27,42 +29,4 @@ module EmbedCallbacks
     end
   end
 
-  class Condition
-    def initialize(options)
-      @if = options[:if]
-      @unless = options[:unless]
-      raise ArgumentError, "Don't use if and unless at the same time." if @if && @unless
-    end
-
-    def call(record)
-      return @if.call(record) if @if 
-      return !@unless&.call(record) if @unless
-      true
-    end
-  end
-
-  class Behavior
-    KINDS = %i(before after around rescue ensure)
-
-    def initialize(behavior)
-      @behavior = behavior
-      raise ArgumentError, 'The behavior should be set in the ' + KINDS.join(' ') unless KINDS.include?(behavior)
-    end
-
-    def before?
-      %i(before around).include?(@behavior)
-    end
-
-    def after?
-      %i(after around).include?(@behavior)
-    end
-
-    def rescue?
-      :rescue == @behavior
-    end
-
-    def ensure?
-      :ensure == @behavior
-    end
-  end
 end
